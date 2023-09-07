3efd94e6fdd690a371b6719710ec7d1b9f1464b0
liu shiwei <liushiwei@gmail.com>
2023-09-07 15:21:04 +0800

    update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo please power on or reset.
+sleep 1
 cd `dirname $0`
 avrdude -v -patmega328p -carduino -P/dev/ttyUSB0 -b57600 -D -Uflash:w:prc.ino.hex:i
 if [ $? != 0 ] ; then
